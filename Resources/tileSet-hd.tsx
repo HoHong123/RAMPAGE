f592e205d1562acb832177e75496c0f956a05633
HoHong123 <30366555+HoHong123@users.noreply.github.com>
2018-12-27 17:24:51 +0900

    Add files via upload

---
+++ Resources/tileSet-hd.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileSet-hd" tilewidth="20" tileheight="20" tilecount="256" columns="20">
+ <image source="tileSet.png" trans="fe80fe" width="256" height="256"/>
+</tileset>
